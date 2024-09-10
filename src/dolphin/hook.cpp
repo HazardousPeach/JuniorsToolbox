c545b8c09628b3aa8d01a89b470dc3e5ff975b1c
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2024-09-09 18:38:34 -0700

    Fix off-by-one error in null-byte insertion for shared mem

--- src/dolphin/hook.cpp
+++ src/dolphin/hook.cpp
@@ -117,7 +117,7 @@ namespace Toolbox::Dolphin {
         char* memory_name_owned = new char[1 + memory_name.size() + 1];
         memory_name_owned[0] = '/';
         std::memcpy(memory_name_owned + 1, memory_name.data(), memory_name.size());
-        memory_name_owned[memory_name.size()] = '\0';
+        memory_name_owned[memory_name.size() + 1] = '\0';
         int fd = shm_open(memory_name_owned, O_RDWR, 0600);
         if (fd == -1) {
             return make_error<Platform::MemHandle>(
